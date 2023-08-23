ab987814082ef737f2540b1c04eb940098c0394e
Ayush chalisee <ayushchalise12@gmail.com>
2023-08-23 10:49:24 +0200

    Define a class called Circle with properties radius and methods to calculate area and circumference.

---
+++ src/main/java/com/java8/Circle.java
@@ -0,0 +1,12 @@
+// Define a class called Circle with properties radius and methods to calculate area and circumference.
+class Scanner  {
+    double radius;
+
+    double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    double calculateCircumference() {
+        return 2 * Math.PI * radius;
+    }
+}
